159de601524f1e499cf33349e5de1e97a0740362
milon19 <milon15-1472@diu.edu.bd>
2019-12-21 10:27:42 +0600

    reINIT

---
+++ app/src/main/java/com/example/blogapplication/Post1Activity.java
@@ -0,0 +1,14 @@
+package com.example.blogapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Post1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post1);
+    }
+}
